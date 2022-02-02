84db58e790b37663d8ba3109f646b99ab126f829
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2022-02-02 21:15:02 +0200

    Annotate add new task

--- app/src/main/java/com/dieantonie/todo/AddNewTask.java
+++ app/src/main/java/com/dieantonie/todo/AddNewTask.java
@@ -35,15 +35,18 @@ public class AddNewTask extends BottomSheetDialogFragment {
 
     @Override
     public void onCreate(@Nullable Bundle saveInstanceState) {
+        // Run bottom sheet DialogFragment onCreate(Bundle)
         super.onCreate(saveInstanceState);
+        // Run bottom sheet DialogFragment setStyle(STYLE_NORMAL, DialogStyle)
         setStyle(STYLE_NORMAL, R.style.DialogStyle);
     }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle saveInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
+        // set view from new_task layout, detached from root.
         View view = inflater.inflate(R.layout.new_task, container, false);
+        // set bottom sheet DialogFragment's dialog window's input mode to SOFT_INPUT_ADJUST_RESIZE.
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
         return view;
@@ -51,32 +54,49 @@ public class AddNewTask extends BottomSheetDialogFragment {
 
     @Override
     public void onViewCreated(View view, Bundle saveInstanceState) {
+        // Run bottom sheet DialogFragment onViewCreated(View, Bundle)
         super.onViewCreated(view, saveInstanceState);
+        // set task text to "new task tex".
         newTaskText = Objects.requireNonNull(getView()).findViewById(R.id.newTaskText);
+        // set task save button to "new task button".
         newTaskSaveButton = getView().findViewById(R.id.newTaskButton);
 
+        // reset is update to false
         boolean isUpdate = false;
 
+        // set bundle to args
         final Bundle bundle = getArguments();
+        // if there are arguments
         if (bundle != null) {
+            // set updated indicator
             isUpdate = true;
+            // get task string from args
             String task = bundle.getString("task");
+            // set task text
             newTaskText.setText(task);
+            // if valid task text
             if (task.length() > 0) {
+                // activate new task save button
                 newTaskSaveButton.setTextColor(ContextCompat.getColor(getContext(), R.color.design_default_color_primary_dark));
             }
         }
 
+        // instantiate new database handler
         db = new DatabaseHandler(getActivity());
+        // set the Writable Database for the database handler.
         db.openDatabase();
 
+        // set text change listner on task text
         newTaskText.addTextChangedListener(new TextWatcher() {
+            // disable before Text Changed event
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
 
+            // override Text Changed event
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // toggle task save button if valid task text
                 if (charSequence.toString().equals((""))) {
                     newTaskSaveButton.setEnabled(false);
                     newTaskSaveButton.setTextColor(Color.GRAY);
@@ -86,24 +106,36 @@ public class AddNewTask extends BottomSheetDialogFragment {
                 }
             }
 
+            // disable after Text Changed event
             @Override
             public void afterTextChanged(Editable editable) {
             }
         });
 
+        // final update check? as opposed to new task
         boolean finalIsUpdate = isUpdate;
+        // set task save button on click listener
         newTaskSaveButton.setOnClickListener(new View.OnClickListener() {
+            // override on click event
             @Override
             public void onClick(View v) {
+                // get task text
                 String text = newTaskText.getText().toString();
+                // validate is update
                 if (finalIsUpdate) {
+                    // update task changes
                     db.updateTask(bundle.getInt("id"), text);
                 } else {
+                    // instantiate new task
                     ToDoModel task = new ToDoModel();
+                    // set new task text
                     task.setTask(text);
+                    // set status unchecked
                     task.setStatus(0);
+                    // persist new task
                     db.insertTask(task);
                 }
+                // run bottom sheet DialogFragment dismiss()
                 dismiss();
             }
         });
@@ -111,8 +143,11 @@ public class AddNewTask extends BottomSheetDialogFragment {
 
     @Override
     public void onDismiss(DialogInterface dialog) {
+        // get activity
         Activity activity = getActivity();
+        // validate if activity is Dialog Close Listener
         if (activity instanceof DialogCloseListener) {
+            // run dialog close listener handler
             ((DialogCloseListener)activity).handleDialogClose(dialog);
         }
     }
