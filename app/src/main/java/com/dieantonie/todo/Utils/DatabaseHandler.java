18c3174f9c21fe3c56b9842fe3bc2f3c10c689b3
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2022-02-11 22:23:30 +0200

    Annotate database handler

--- app/src/main/java/com/dieantonie/todo/Utils/DatabaseHandler.java
+++ app/src/main/java/com/dieantonie/todo/Utils/DatabaseHandler.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import com.dieantonie.todo.Model.ToDoModel;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +27,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // Create table
         db.execSQL(CREATE_TODO_TABLE);
     }
 
@@ -41,55 +40,83 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     public void openDatabase() {
+        // set database
         db = this.getWritableDatabase();
     }
 
     public void insertTask(ToDoModel task) {
+        // declare content values
         ContentValues cv = new ContentValues();
+        // Set content value task
         cv.put(TASK, task.getTask());
+        // set content value task status
         cv.put(STATUS, 0);
+        // Persist task data
         db.insert(TODO_TABLE, null, cv);
     }
 
     public List<ToDoModel> getAllTasks() {
+        // Declare task list
         List<ToDoModel> taskList = new ArrayList<>();
+        // Declare cursor
         Cursor cur = null;
+        // begin database transaction
         db.beginTransaction();
         try {
+            // Query table
             cur = db.query(TODO_TABLE, null, null, null, null, null, null, null);
+            // Check if data returned
             if (cur != null) {
+                // Start cursor from top
                 if (cur.moveToFirst()) {
                     do {
+                        // Instantiate task
                         ToDoModel task = new ToDoModel();
+                        // Set task id from retrieved data
                         task.setId(cur.getInt(cur.getColumnIndex(ID)));
+                        // Set task text from retrieved data
                         task.setTask(cur.getString(cur.getColumnIndex(TASK)));
+                        // Set task status from retrieved data
                         task.setStatus(cur.getInt(cur.getColumnIndex(STATUS)));
+                        // Add task to task list
                         taskList.add(task);
+                    // continue to next database item
                     } while (cur.moveToNext());
                 }
             }
         }
         finally {
+            // close database transaction
             db.endTransaction();
+            // check cursor is not null?
             assert cur != null;
+            // close cursor
             cur.close();
         }
+        // return compiled task list
         return taskList;
     }
 
     public void updateStatus(int id, int status) {
+        // Declare content values
         ContentValues cv = new ContentValues();
+        // update task status
         cv.put(STATUS, status);
+        // Persist changes for task
         db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf((id))});
     }
 
     public void updateTask(int id, String task) {
+        // Declare content values
         ContentValues cv = new ContentValues();
+        // update task text
         cv.put(TASK, task);
+        // Persist changes for task
         db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf((id))});
     }
 
     public void deleteTask(int id) {
+        // Remove task from persistence
         db.delete(TODO_TABLE, ID + "= ?", new String[] {String.valueOf((id))});
     }
 }
